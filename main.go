f6138f365b88a153cca7c9bdecd8f6eac44d63c6
Clement Bogyah <Zena@Clements-MacBook-Pro.local>
2022-10-29 17:33:55 +0000

    changes

--- main.go
+++ main.go
@@ -1,24 +1,41 @@
-// Package declearation always first, it's a convention to always use package main
+// Declaration
 package main
 
-import "log"
+import "fmt"
 
-// Every go package must consist of at least on function i.e main function
-// Functions are declared with "func" keyword
-// Unitialized int is asigned to 0
-// _ means ignore that value
-
+// package level declaration
+const boilingF =212.0
 func main(){
-	// Variable declaration
+	// local level declaration
+	var f = boilingF
+	var c = (f-32)*5/9
+	fmt.Printf("boiling point = %g F or %g C\n", f,c)
+
+	// var name type = expression
+	// if no initializer then int = 0, string ="",boolean = false
+	var myName string = "Clement"
+	fmt.Println(myName)
+
+	// This is a short declaration
+	i := 100
+	fmt.Println(i)
+
+	// Pointers: A pointer value is the address of a variable ,&x is (address of x)
+	x := 1
+	p := &x //p , of type int , points to x
+	fmt.Println(p , &x) // p = 1
+	*p = 2
+	fmt.Println(x) // 2
+
+	// The new function : new(T) create unnamed variable of type T, init to 0 and return address which is the type of T
+
+	n := new(int) // n, of type int , points to an unnamed int variable
+	fmt.Println(*n)
+	*n = 2 //set unnamed int to 2
+	fmt.Println(*n)
+
+
 	
-	var whatToSay string
-	whatToSay,_= saySomething("Hello world")
-
-	log.Println(whatToSay)
 
 }
 
-
-func saySomething(s string) (string,string){
-	return s, "Goodbye"
-}
